8291ab0ec81a1e2395bc9a42ae21475fb8735eac
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-04-16 21:36:24 -0500

    Set Reaction Schema

---
+++ models/Reaction.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formateDate
+        }
+    },
+    {
+      toJSON: {getters: true},
+      id: false,
+    }
+);
+
+function formateDate (createdAt) {
+    const date = new Date(createdAt);
+    const month = date.toLocaleString('default', { month: 'short' });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const time = date.toLocaleTimeString()
+    console.log(`${month} ${day}, ${year} at ${time}`);
+    return `${month} ${day}, ${year} at ${time}`
+};
+
+module.exports = reactionSchema
