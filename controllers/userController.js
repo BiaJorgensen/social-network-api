61a2ab96c0855545e181bd7899dbba485114cde9
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-04-20 17:06:50 -0500

    Fixed typo

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ module.exports = {
 
     // Add Friend to User using $addToset
     addFriend(req, res) {
-        User.findOneAndUpdate({ _id: req.params.userId }, { $addToset: { friends: req.params.friendId} }, { runValidators: true, new: true })
+        User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId} }, { runValidators: true, new: true })
         .then((user) => user ? res.json(user) : res.status(404).json({ message: 'No user found with that ID' }))
         .catch((err) => res.status(500).json(err))
     },
